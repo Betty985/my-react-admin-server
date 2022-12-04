5d6cb06d27d840200a28bd8a0b376b79a7a37714
Betty985 <1830265753@qq.com>
2022-12-04 23:48:43 +0800

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const appOptions={cors:true}
+  const app = await NestFactory.create(AppModule,appOptions);
+  const options=new DocumentBuilder().setTitle('Mestjs').addBearerAuth().build()
+  const document=SwaggerModule.createDocument(app,options)
+  SwaggerModule.setup('/',app,document)
   await app.listen(3000);
 }
 bootstrap();
